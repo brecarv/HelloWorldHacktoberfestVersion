8fe25e9fe1a338303a3c82ac7f983dd7971832c5
Akshita Sah <akshita.asah@gmail.com>
2019-10-12 14:14:04 +0530

    Added Hello World in JAVA language

---
+++ HelloWorld-akshi0409.java.java
@@ -0,0 +1,8 @@
+﻿public class HelloWorld-akshi0409
+ {
+  public static void main(String[] args) 
+   {
+    System.out.println("Hi, I am Akshita Sah and I use the language JAVA.");
+    System.out.println("Hello World");
+   }
+}
